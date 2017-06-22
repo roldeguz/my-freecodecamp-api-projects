5593724f5b8f77094ed131d1add566f776018c14
System <none>
2017-06-22 19:29:04 +0000

    🐾🚘 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,11 @@
+// public/client.js
+// roldeguz
+// 22-June-2017
+// freecodecamp API projects: timestamp microservice
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+
+$(function() {
+  console.log('hello world :o');    
+});
